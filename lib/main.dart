cd03f205578c191be68ec02c524e5e26566e2164
shohei <hukusuke1007@gmail.com>
2023-07-13 19:02:14 +0900

    refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_sample/app.dart';
+import 'package:flutter_sample/repositories/local_database/local_database_repository.dart';
 import 'package:flutter_sample/repositories/local_database/shared_preferences/bridge/shared_preferences_repository_3.dart'
     as spr3;
+import 'package:flutter_sample/repositories/local_database/shared_preferences/bridge/shared_preferences_repository_4.dart'
+    as spr4;
+import 'package:flutter_sample/use_cases/counter/flutter_bloc/counter_controller.dart'
+    as bloc_counter;
 import 'package:flutter_sample/utils.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final sharedPreferences = await SharedPreferences.getInstance();
+  const flutterSecureStorage = FlutterSecureStorage();
 
   /// Get it
-  setUp();
+  getIt
+    ..registerLazySingleton<SharedPreferences>(
+      () => sharedPreferences,
+    )
+    ..registerLazySingleton<FlutterSecureStorage>(
+      () => flutterSecureStorage,
+    )
+    ..registerLazySingleton<LocalDatabaseRepository>(
+      spr4.SharedPreferencesRepository.new,
+    )
+    ..registerFactory<bloc_counter.CounterController>(
+      bloc_counter.CounterController.new,
+    );
 
   /// Riverpod
-  final sharedPreferences = await SharedPreferences.getInstance();
   runApp(
     ProviderScope(
       overrides: [
         sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+        flutterSecureStorageProvider.overrideWithValue(flutterSecureStorage),
         localDatabaseRepositoryProvider
             .overrideWith(spr3.SharedPreferencesRepository.new),
       ],
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,37 +1,18 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_sample/repositories/local_database/local_database_repository.dart';
-import 'package:flutter_sample/repositories/local_database/shared_preferences/bridge/shared_preferences_repository_4.dart'
-    as get_it;
-import 'package:flutter_sample/use_cases/counter/flutter_bloc/counter_controller.dart'
-    as flutter_bloc;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 /// Get it
 final getIt = GetIt.instance;
-void setUp() {
-  getIt
-    ..registerSingletonAsync<SharedPreferences>(
-      SharedPreferences.getInstance,
-    )
-    ..registerLazySingleton<FlutterSecureStorage>(
-      () => const FlutterSecureStorage(),
-    )
-    ..registerLazySingleton<LocalDatabaseRepository>(
-      get_it.SharedPreferencesRepository.new,
-    )
-    ..registerFactory<flutter_bloc.CounterController>(
-      flutter_bloc.CounterController.new,
-    );
-}
 
-/// Provider
+/// Riverpod Provider
 final sharedPreferencesProvider = Provider<SharedPreferences>(
   (_) => throw UnimplementedError(),
 );
 final flutterSecureStorageProvider = Provider<FlutterSecureStorage>(
-  (_) => const FlutterSecureStorage(),
+  (_) => throw UnimplementedError(),
 );
 final localDatabaseRepositoryProvider = Provider<LocalDatabaseRepository>(
   (_) => throw UnimplementedError(),
