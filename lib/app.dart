a15b3d398a936de602d48ece4e56f7f8d629fa2d
shohei <hukusuke1007@gmail.com>
2023-07-17 09:38:16 +0900

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -4,7 +4,6 @@ import 'package:flutter_sample/pages/main_page.dart';
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
